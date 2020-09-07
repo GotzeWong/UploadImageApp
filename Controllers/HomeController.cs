2a8d10eb64f692b58acc68efbaa9476370da4de7
Michael Wang <wanggechao@gmail.com>
2020-09-07 15:20:03 +0800

    First to init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UploadImageApp.Models;
+
+namespace UploadImageApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("/GeneralInterview/UploadLogo")]
+        [HttpPost]
+        public JsonResult UploadFile(List<IFormFile> files)
+        {
+            List<string> fileNames = new List<string>();
+
+            foreach(IFormFile file in files)
+                fileNames.Add(file.FileName);
+            return Json(fileNames);
+        }
+
+    }
+}
